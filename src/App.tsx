1f731311d8bfc7a91701aaf12a70db71f5ae1274
Jaehun <qkdwogns98@gmail.com>
2024-12-01 19:32:53 +0900

    chore: reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,5 @@
-import { useState } from 'react'
-
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <button type="button" onClick={() => setCount((count) => count + 1)}>
-        App
-      </button>
-      <p>{count}</p>
-    </>
-  )
+  return <>App</>
 }
 
 export default App
